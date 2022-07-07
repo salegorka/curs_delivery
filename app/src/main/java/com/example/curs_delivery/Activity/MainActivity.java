dd0f8f17ad326bb0e842870fdee641c490a76514
egor_salomatin <salomatin.e@introvert.bz>
2022-07-07 17:42:30 +0500

    Commit

--- app/src/main/java/com/example/curs_delivery/Activity/MainActivity.java
+++ app/src/main/java/com/example/curs_delivery/Activity/MainActivity.java
@@ -1,37 +1,53 @@
 package com.example.curs_delivery.Activity;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
-import com.example.curs_delivery.Domain.CategoryDomain;
+import com.example.curs_delivery.Adapter.ProductCategoryAdapter;
+import com.example.curs_delivery.Model.ProductCategory;
 import com.example.curs_delivery.R;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private RecyclerView.Adapter adapter;
-    private RecyclerView recyclerViewCategoryList;
+    ProductCategoryAdapter productCategoryAdapter;
+    RecyclerView productCatRecycler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_intro);
+        setContentView(R.layout.activity_main);
+        Intent intent = getIntent();
+
+        List<ProductCategory> productCategoryList = new ArrayList<>();
+        productCategoryList.add(new ProductCategory(1, "Категория 1"));
+        productCategoryList.add(new ProductCategory(2, "Категория 2"));
+        productCategoryList.add(new ProductCategory(3, "Категория 3"));
+        productCategoryList.add(new ProductCategory(4, "Категория 4"));
+        setProductCatRecycler(productCategoryList);
     }
 
-    private void recyclerViewCategory() {
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
-        recyclerViewCategoryList = findViewById(R.id.recyclerView);
-        recyclerViewCategoryList.setLayoutManager(linearLayoutManager);
+    private void setProductCatRecycler(List<ProductCategory> productCategoryList) {
+        productCatRecycler = findViewById(R.id.cat_recycler);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, RecyclerView.HORIZONTAL, false);
+        productCatRecycler.setLayoutManager(layoutManager);
+        productCategoryAdapter = new ProductCategoryAdapter(this, productCategoryList);
+        productCatRecycler.setAdapter(productCategoryAdapter);
+    }
 
-        ArrayList<CategoryDomain> category = new ArrayList<>();
-        category.add(new CategoryDomain("Pizza", "cat_1"));
-        category.add(new CategoryDomain("Burger", "cat_2"));
-        category.add(new CategoryDomain("Holiday", "cat_3"));
-        category.add(new CategoryDomain("Drink", "cat_4"));
-        category.add(new CategoryDomain("Donut", "cat_5"));
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.my_menu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
