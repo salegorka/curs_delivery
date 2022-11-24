a0cc90270a6a5c6092a400a3047d4a2b6ed8a97a
salegorka <egorka.salomatin@yandex.ru>
2022-11-24 22:49:47 +0500

    commit

--- app/src/main/java/com/example/curs_delivery/Activity/CartActivity.java
+++ app/src/main/java/com/example/curs_delivery/Activity/CartActivity.java
@@ -1,11 +1,15 @@
 package com.example.curs_delivery.Activity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,11 +23,16 @@ import com.example.curs_delivery.Database.CartDao;
 import com.example.curs_delivery.Model.Cart;
 import com.example.curs_delivery.R;
 
+import java.util.Calendar;
 import java.util.List;
 
 
-public class CartActivity extends AppCompatActivity {
+public class CartActivity extends AppCompatActivity implements View.OnClickListener {
     AppDatabase db;
+
+    Button btnDatePicker, btnTimePicker;
+    EditText txtDate, txtTime;
+    private int mYear, mMonth, mDay, mHour, mMinute;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,8 +40,37 @@ public class CartActivity extends AppCompatActivity {
         setContentView(R.layout.activity_cart);
         db = App.getInstance().getDatabase();
 
+        btnDatePicker = (Button)findViewById(R.id.buttonDatePickCart);
+        btnTimePicker = (Button)findViewById(R.id.buttonTimePickCart);
+        txtDate = (EditText)findViewById(R.id.editTextDateCart);
+        txtTime = (EditText)findViewById(R.id.editTextTimeCart);
 
+        btnDatePicker.setOnClickListener(this);
+        btnTimePicker.setOnClickListener(this);
     }
+
+    public void onClick(View v) {
+        if (v == btnDatePicker) {
+            final Calendar calendar = Calendar.getInstance();
+            mYear = calendar.get(Calendar.YEAR);
+            mMonth = calendar.get(Calendar.MONTH);
+            mDay = calendar.get(Calendar.DAY_OF_MONTH);
+
+            DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker datePicker, int year, int month, int day_of_month) {
+                    txtDate.setText(year + "-" + (month + 1) + "-" + day_of_month);
+                }
+            }, mYear, mMonth, mDay);
+            datePickerDialog.show();
+        }
+        /*
+        if (v == btnTimePicker) {
+
+        }
+         */
+    }
+
 
     @Override
     protected void onResume() {
